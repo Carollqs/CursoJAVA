adbe3eb7c0b0c76d82afd808eeb850d32836c0bb
Carolina Queiroz <saracarolina.queiroz@gmail.com>
2024-06-29 14:43:16 -0300

    update

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -3,3 +3,16 @@ public class TiposVariaveis {
         System.out.println("Hello, World!");
     }
 }
+
+
+/*
+   -> Letra do tipo no final do valor da val <-
+ -> se for do tipo FLOAT, 
+
+  float pi = 3.14F;
+
+  -> LONG,
+   
+  long cpf = 56755713498L;
+
+ */
